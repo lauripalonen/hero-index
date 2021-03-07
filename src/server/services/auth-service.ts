00a4af91dc75fb51758890e667ab047f07f4db65
lauripalonen <palonen.lauri@gmail.com>
2021-03-07 21:26:46 +0200

    streamline authChecker

--- src/server/services/auth-service.ts
+++ src/server/services/auth-service.ts
@@ -2,22 +2,20 @@ import * as jwt from 'jsonwebtoken';
 import { AuthChecker } from 'type-graphql'
 import { Hero } from '../entities/hero'
 
-const JWT_SIGNING_SECRET = process.env.JWT_SECRET;
+const generateJwtForUserId = (userId: string) => jwt.sign(
+	{ userId }, process.env.JWT_SECRET
+);
 
-const generateJwtForUserId = (userId: string) => jwt.sign({ userId }, JWT_SIGNING_SECRET);
+const customAuthChecker: AuthChecker<Hero> = async ({ context }, roles) => {
+	const requiredRole = roles[0]
+	const userRoles = (await context.roles).map(role => role.title)
 
-const customAuthChecker: AuthChecker<Hero> = async (
-	{ root, args, context, info },
-	roles,
-) => {
-	const userRoles = await context.roles
-	const roleTitles = userRoles.map(role => role.title)
-	if (roleTitles.includes('TREASURER')) {
+	if (userRoles.includes(requiredRole)) {
 		return true
 	}
+
 	return false
 }
-
 
 export const AuthService = () => ({
 	generateJwtForUserId,
